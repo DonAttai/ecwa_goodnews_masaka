80ee8449ed38669188a488be6f2af70045efabfc
John Attai Yinusa <donattai1032@gmail.com>
2026-06-24 15:27:18 +0100

    remove custom url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,5 @@
 import "dotenv/config"
 import { defineConfig } from "prisma/config"
-const url =
-  process.env.NODE_ENV === "development"
-    ? process.env["DATABASE_URL"]
-    : process.env["DIRECT_URL"]
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
